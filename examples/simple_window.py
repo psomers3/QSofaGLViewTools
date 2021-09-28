6fa5bef08236ec574a67e62c34b9c05d49cfd2b1
psomers3 <peter.somers@isys.uni-stuttgart.de>
2021-09-28 17:12:17 +0200

    cleaned example

--- examples/simple_window.py
+++ examples/simple_window.py
@@ -14,32 +14,22 @@ def create_scene(root):
     importPlugin("SofaMiscForceField")
     importPlugin("SofaBaseLinearSolver")
     importPlugin("SofaBaseMechanics")
+
     root.gravity = [0, -1., 0]
     root.addObject("VisualStyle", displayFlags="showBehaviorModels showAll")
-    root.addObject("MeshGmshLoader", name="meshLoaderCoarse",
-                   filename="../test/liver.msh")
-    root.addObject("MeshObjLoader", name="meshLoaderFine",
-                   filename="mesh/liver-smooth.obj")
-
+    root.addObject("MeshGmshLoader", name="meshLoaderCoarse", filename="../test/liver.msh")
+    root.addObject("MeshObjLoader", name="meshLoaderFine", filename="mesh/liver-smooth.obj")
     root.addObject("EulerImplicitSolver")
-    root.addObject("CGLinearSolver", iterations="200",
-                   tolerance="1e-09", threshold="1e-09")
+    root.addObject("CGLinearSolver", iterations="200", tolerance="1e-09", threshold="1e-09")
 
     liver = root.addChild("liver")
-
-    liver.addObject("TetrahedronSetTopologyContainer",
-                    name="topo", src="@../meshLoaderCoarse")
-    liver.addObject("TetrahedronSetGeometryAlgorithms",
-                    template="Vec3d", name="GeomAlgo")
-    liver.addObject("MechanicalObject",
-                    template="Vec3d",
-                    name="MechanicalModel", showObject="1", showObjectScale="3")
-
-    liver.addObject("TetrahedronFEMForceField", name="fem", youngModulus="1000",
-                    poissonRatio="0.4", method="large")
-
+    liver.addObject("TetrahedronSetTopologyContainer", name="topo", src="@../meshLoaderCoarse")
+    liver.addObject("TetrahedronSetGeometryAlgorithms", template="Vec3d", name="GeomAlgo")
+    liver.addObject("MechanicalObject", template="Vec3d", name="MechanicalModel", showObject="1", showObjectScale="3")
+    liver.addObject("TetrahedronFEMForceField", name="fem", youngModulus="1000", poissonRatio="0.4", method="large")
     liver.addObject("MeshMatrixMass", massDensity="1")
     liver.addObject("FixedConstraint", indices="2 3 50")
+
     visual = liver.addChild("visual")
     visual.addObject('MeshObjLoader', name="meshLoader_0", filename="mesh/liver-smooth.obj", handleSeams="1")
     visual.addObject('OglModel', name="VisualModel", src="@meshLoader_0", color='red')
@@ -51,23 +41,27 @@ def create_scene(root):
 
 
 def main(node: Sofa.Core.Node, viewer: QSofaGLView):
+    """
+    Main function that will run as the typical python script
+    Parameters
+    ----------
+    node : Sofa.Core.Node
+    viewer : QSofaGLView
+    """
     viewer.set_background_color([0,0,0,1])
-    node.init()
-    Sofa.Simulation.updateVisual(node)
     input("\nPress enter to simulate 10 seconds:")
     start = time.time()
     last = start
     while time.time() - start < 10:
         Sofa.Simulation.animate(node, time.time() - last)
-        Sofa.Simulation.updateVisual(node)
+        Sofa.Simulation.updateVisual(node)  # idk why this isn't working right now.
         last = time.time()
         time.sleep(0.01)
     input("\nPress enter to quit:")
-    viewer.hide()
     viewer.close()
 
 
 if __name__ == '__main__':
-    root_node = Sofa.Core.Node("Root")
-    create_scene(root_node)
-    create_simple_window(main, root_node)
+    root_node = Sofa.Core.Node("Root")  # create root node
+    create_scene(root_node)  # fill the scene
+    create_simple_window(main, root_node)  # create a window and call the main function
